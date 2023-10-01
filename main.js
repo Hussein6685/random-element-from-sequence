3c05b95247e12b1c74ff8e99a1984789ec5c0b9a
Hussein6685 <113841959+Hussein6685@users.noreply.github.com>
2023-10-01 17:41:23 +0300

    html & js done

---
+++ main.js
@@ -0,0 +1,11 @@
+let arr = ["Ahmed", "Sayed", "Ali"];
+
+console.log(arr.length);
+
+
+console.log(Math.random() * arr.length);
+
+console.log(Math.floor(Math.random() * arr.length));
+console.log(Math.trunc(Math.random() * arr.length));
+
+console.log(arr[Math.trunc(Math.random() * arr.length)]);
